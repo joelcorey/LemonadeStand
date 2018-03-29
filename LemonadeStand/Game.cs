516f2a594afbb0b58a84bcdad03f1aabfed7c6cf
Joel Corey <joelcorey@fastmail.com>
2018-03-29 07:26:02 -0600

    Init

---
+++ LemonadeStand/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+
+        Game game = new Game();
+
+    }
+}
