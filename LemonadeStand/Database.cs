a8b0d9c9a1606ad7c6285c6d7b6908a09e070fca
Joel Corey <joelcorey@fastmail.com>
2018-03-30 15:21:25 -0600

    Database inherits from APIKey

--- LemonadeStand/Database.cs
+++ LemonadeStand/Database.cs
@@ -7,24 +7,20 @@ using System.Net.Http;
 
 namespace LemonadeStand
 {
-    class Database
+    class Database : APIKey
     {
         public async void DatabaseConnect()
         {
-            //var client = new HttpClient();
-            //var values = new List<KeyValuePair<string, string>>();
-            //values.Add(new KeyValuePair<string, string>("task", task));
-            //values.Add(new KeyValuePair<string, string>("merchant", merchant_id));
-            //values.Add(new KeyValuePair<string, string>("ref", ref);
-            //// include other fields
-            //var content = new FormUrlEncodedContent(values);
-            //var response = await client.PostAsync("https://voguepay.com/api/", content);
+            var client = new HttpClient();
+            var values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>("task", task));
+            values.Add(new KeyValuePair<string, string>("merchant", merchant_id));
+            values.Add(new KeyValuePair<string, string>("ref", ref);
+            // include other fields
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync("https://voguepay.com/api/", content);
 
-            //using (var client = new HttpClient())
-            //{
-            //    var result = await client.GetAsync("https://www.google.com");
-            //    Console.WriteLine(result.StatusCode);
-            //}
+            
         }
 
         private void DatabaseSetup()
